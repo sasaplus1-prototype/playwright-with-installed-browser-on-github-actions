855d9f1aa523af18398086c984c9262dc8de3e7a
sasaplus1 <sasaplus1@gmail.com>
2022-06-11 13:12:52 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# playwright-with-installed-browser-on-github-actions
+
+playwright with installed browser on GitHub Actions
+
+## License
+
+The MIT license
---
+++ playwright.config.ts
@@ -0,0 +1,35 @@
+import os from 'os';
+
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chrome',
+      use: {
+        channel: 'chrome'
+      },
+    },
+    {
+      name: 'msedge',
+      use: {
+        channel: 'msedge'
+      },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ],
+};
+
+export default config;
