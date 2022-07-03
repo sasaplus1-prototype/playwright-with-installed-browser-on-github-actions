f0067655d4ed5c5b159331427cf6b619ca3fdbd8
sasaplus1 <sasaplus1@gmail.com>
2022-07-03 15:23:56 +0900

    fix

--- tests/input.spec.ts
+++ tests/input.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://d-toybox.com/studio/lib/input_event_viewer.html');
   const editor = page.locator('#editor');
-  await page.keyboard.press('\u0015');
+  // await page.keyboard.press('\u0015');
   await page.keyboard.press('a');
   await page.keyboard.press('i');
   await page.keyboard.press('u');
