92302ac873efec83f3e7671098c6a32404fda886
sasaplus1 <sasaplus1@gmail.com>
2022-07-03 15:22:40 +0900

    add keystroke test

---
+++ tests/input.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://d-toybox.com/studio/lib/input_event_viewer.html');
+  const editor = page.locator('#editor');
+  await page.keyboard.press('\u0015');
+  await page.keyboard.press('a');
+  await page.keyboard.press('i');
+  await page.keyboard.press('u');
+  await page.keyboard.press('e');
+  await page.keyboard.press('o');
+  await page.screenshot({ path: `input.png` });
+});
